7d7bd3e7b283f98f52b4d5350b386cbaeea5f3ff
doguykt <doguykt32@gmail.com>
2025-10-15 02:52:27 +0300

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,9 +1,1 @@
-cd $RENDER_PROJECT_ROOT || exit 1
 #!/bin/bash
-# Komutu çalıştırmadan önce, deponun kök dizinine geçiyoruz
-# Bu, Render'ın ortamına göre dosya yolunu düzeltir
-cd $RENDER_PROJECT_ROOT || exit 1 
-
-pip install --upgrade pip
-pip install -r requirements.txt
-python trend_fiyat_bot_final.py
